dc499932de1afdbb0228905bb280fb7860bc8db9
David <davidmollera@hotmail.com>
2022-12-18 19:11:09 -0500

    installed firebase and created a folder with init.js to store the firebase info

---
+++ src/firebase/init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAI5-kFiubDJBEReDrRebEz6xk5vjXGOmU",
+  authDomain: "fir-practice-fb16e.firebaseapp.com",
+  projectId: "fir-practice-fb16e",
+  storageBucket: "fir-practice-fb16e.appspot.com",
+  messagingSenderId: "563602174772",
+  appId: "1:563602174772:web:16e96cbdcdec370a872b61"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
