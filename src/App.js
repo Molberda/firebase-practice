e2b1d299f582c425989f84fc80e26cdade8d2bad
David <davidmollera@hotmail.com>
2022-12-18 22:19:06 -0500

    defined the register fucntion with the instructions from firebase documentation

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import './App.css';
 
 function App() {
   function register(){
-     
+    createUserWithEmailAndPassword(auth, "email@gmail.com", "test1234")
+    .then((user) => {
+      console.log(user)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
   }
   return (
     <div className="App">
