6943fc1905bc6fdc78ec7e41730302cfaeb195f4
David <davidmollera@hotmail.com>
2022-12-22 18:50:12 -0500

    added a photo to the assets folder and imported it the logprofile component

--- src/Ui/LogProfile.jsx
+++ src/Ui/LogProfile.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import ProfilePic from '../assets/foto__david.jpeg'
 
 const LogProfile = () => {
     return (
-        <div>
-            
+        <div className='profile__wrapper'>
+            <img src="" alt="" className="profile__pic" />
         </div>
     );
 }
