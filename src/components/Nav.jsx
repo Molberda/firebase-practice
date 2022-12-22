6bede9d35a7e3b79d21fce5caaad0f3336e0284c
David <davidmollera@hotmail.com>
2022-12-21 19:12:59 -0500

    imoprted, auth, react, useeffect, usestate, create userwithemailandpassword, signout,etc in nav component

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,11 @@
 import React from 'react';
+import { auth } from "./firebase/init.js"
+import { 
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword, 
+  signOut,
+  onAuthStateChanged } from "firebase/auth";
+import React, { useEffect, useState } from "react";
 
 const Nav = () => {
     return (
