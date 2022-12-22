3576f7a8f2bb9b4b4f542f2782c91fc45cce60c8
David <davidmollera@hotmail.com>
2022-12-21 19:31:23 -0500

    created html for the nav component

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,55 +1,58 @@
-import React from 'react';
-import { auth } from "../firebase/init.js"
-import { 
+import React from "react";
+import { auth } from "../firebase/init.js";
+import {
   createUserWithEmailAndPassword,
-  signInWithEmailAndPassword, 
+  signInWithEmailAndPassword,
   signOut,
-  onAuthStateChanged } from "firebase/auth";
+  onAuthStateChanged,
+} from "firebase/auth";
 import { useEffect, useState } from "react";
 
 const Nav = () => {
-
-    const [user, setUser] = useState({});
+  const [user, setUser] = useState({});
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    onAuthStateChanged(auth, (user)=> {
-      setLoading(false)
-      if (user){
-        setUser(user)
+    onAuthStateChanged(auth, (user) => {
+      setLoading(false);
+      if (user) {
+        setUser(user);
       }
-    })
-   }, []);
+    });
+  }, []);
 
-  function register(){
+  function register() {
     createUserWithEmailAndPassword(auth, "email@gmail.com", "test1234")
-    .then((data) => {
-      console.log(data)
-    })
-    .catch((error) => {
-      console.log(error.message)
-    })
-  }
-
-    function login(){
-      signInWithEmailAndPassword(auth, "email@gmail.com", "test1234")
       .then((data) => {
-        setUser(data.user)
+        console.log(data);
       })
       .catch((error) => {
-        console.log(error.message)
-      })
-    }
+        console.log(error.message);
+      });
+  }
 
-    function logout(){
-      signOut(auth)
-      setUser({})
-    }
-    return (
-        <div>
-            
-        </div>
-    );
-}
+  function login() {
+    signInWithEmailAndPassword(auth, "email@gmail.com", "test1234")
+      .then((data) => {
+        setUser(data.user);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  }
+
+  function logout() {
+    signOut(auth);
+    setUser({});
+  }
+  return (
+    <div>
+      <button onClick={register}>Register</button>
+      <button onClick={login}>LogIn</button>
+      <button onClick={logout}>LogOut</button>
+      {loading ? "loading..." : user.email}
+    </div>
+  );
+};
 
 export default Nav;
